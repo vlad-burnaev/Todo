92587370ec7e0700920eb75243fe4a49dc954c46
Vlad Burnaev <vlad_burn@icloud.com>
2020-10-21 13:32:20 +0300

    Updates

---
+++ precache-manifest.ca886e792b5ae02ada41a5649164c993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64abad3a11336af1e8dd1913d7fbc3dc",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "2feabebc853efbb3137f",
+    "url": "/Todo/static/css/main.0a9f5958.chunk.css"
+  },
+  {
+    "revision": "57e789189e7f5d121144",
+    "url": "/Todo/static/js/2.342d4d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.342d4d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee874dac18921c7967b",
+    "url": "/Todo/static/js/3.01a9ec99.chunk.js"
+  },
+  {
+    "revision": "2feabebc853efbb3137f",
+    "url": "/Todo/static/js/main.25f9ca13.chunk.js"
+  },
+  {
+    "revision": "0fdc200fa944a52a859a",
+    "url": "/Todo/static/js/runtime-main.75ccb8d1.js"
+  }
+]);
